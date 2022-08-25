0f5144605de5f0383887ce47c948de9c4da15b63
Aeolus.Zhang <aeolus.zhang@fanruan.com>
2022-08-25 20:35:08 +0800

    select组件

--- packages/components/stories/Select/Select.stories.tsx
+++ packages/components/stories/Select/Select.stories.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Select from "./index";
+import { DataType, Select } from "./Select";
 
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+const people:DataType[] = [
+  { name: "Wade Cooper", value: 1 },
+  { name: "Arlene Mccoy", value: 2 },
+  { name: "Devon Webb", value: 3 },
+  { name: "Tom Cook", value: 4 },
+  { name: "Tanya Fox", value: 5 },
+  { name: "Hellen Schmidt", value: 6 },
+];
+
+const basicList:DataType[] = [
+  { name: "Option1", value: 1 },
+  { name: "Option2", value: 2 },
+];
 
 export default {
   title: "Component/Select",
-  compoent: Select,
+  component: Select,
 } as ComponentMeta<typeof Select>;
 
 const Template: ComponentStory<typeof Select> = (args) => {
-  return <Select />;
+  const [selected, setSelected] = useState(basicList[0]);
+
+  return (
+    <Select {...args} onChange={(e) => setSelected(e)} selected={selected} />
+  );
 };
 
-export const Basic = Template.bind({});
-Basic.args = {};
+export const Primary = Template.bind({});
+
+Primary.args = {
+  data: basicList,
+};
